9b866b9e00ef2ddd0774130d26a80d843ad78c62
Teniola Adeyemi <teniolaa890@gmail.com>
2025-09-02 13:16:17 +0100

    Update Script.js

--- Calculator/Script.js
+++ Calculator/Script.js
@@ -1,13 +1,12 @@
 let calculation = localStorage.getItem('calculation') || '';
 
-// Display the calculation when the page first loads.
+
 displayCalculation();
 
 function updateCalculation(value) {
     calculation += value;
 
-    // Display the calculation on the page
-    // instead of console.log
+    
     displayCalculation();
 
     localStorage.setItem('calculation', calculation);
@@ -16,4 +15,5 @@ function updateCalculation(value) {
 function displayCalculation() {
     document.querySelector('.js-calculation')
         .value = calculation;
+
 }
